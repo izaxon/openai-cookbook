d063b607f414aeaebdab5556d67f604a5ad4e8b2
Johan Isaksson <johan@izaxon.com>
2023-02-04 10:24:57 +0100

    Adapted to international characters support. Infrakraft test. Git ignore updated.

--- solutions/web_crawl_Q&A/web-qa.py
+++ solutions/web_crawl_Q&A/web-qa.py
@@ -1,5 +1,5 @@
 ################################################################################
-### Step 1
+# Step 1
 ################################################################################
 
 import requests
@@ -22,10 +22,12 @@ from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
 HTTP_URL_PATTERN = r'^http[s]*://.+'
 
 # Define root domain to crawl
-domain = "openai.com"
-full_url = "https://openai.com/"
+domain = "www.infrakraft.se"
+full_url = "https://www.infrakraft.se/"
 
 # Create a class to parse the HTML and get the hyperlinks
+
+
 class HyperlinkParser(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -41,12 +43,14 @@ class HyperlinkParser(HTMLParser):
             self.hyperlinks.append(attrs["href"])
 
 ################################################################################
-### Step 2
+# Step 2
 ################################################################################
 
 # Function to get the hyperlinks from a URL
+
+
 def get_hyperlinks(url):
-    
+
     # Try to open the URL and read the HTML
     try:
         # Open the URL and read the HTML
@@ -55,7 +59,7 @@ def get_hyperlinks(url):
             # If the response is not HTML, return an empty list
             if not response.info().get('Content-Type').startswith("text/html"):
                 return []
-            
+
             # Decode the HTML
             html = response.read().decode('utf-8')
     except Exception as e:
@@ -69,10 +73,12 @@ def get_hyperlinks(url):
     return parser.hyperlinks
 
 ################################################################################
-### Step 3
+# Step 3
 ################################################################################
 
 # Function to get the hyperlinks from a URL that are within the same domain
+
+
 def get_domain_hyperlinks(local_domain, url):
     clean_links = []
     for link in set(get_hyperlinks(url)):
@@ -103,7 +109,7 @@ def get_domain_hyperlinks(local_domain, url):
 
 
 ################################################################################
-### Step 4
+# Step 4
 ################################################################################
 
 def crawl(url):
@@ -118,37 +124,48 @@ def crawl(url):
 
     # Create a directory to store the text files
     if not os.path.exists("text/"):
-            os.mkdir("text/")
+        os.mkdir("text/")
 
     if not os.path.exists("text/"+local_domain+"/"):
-            os.mkdir("text/" + local_domain + "/")
+        os.mkdir("text/" + local_domain + "/")
 
     # Create a directory to store the csv files
     if not os.path.exists("processed"):
-            os.mkdir("processed")
+        os.mkdir("processed")
 
     # While the queue is not empty, continue crawling
     while queue:
 
         # Get the next URL from the queue
         url = queue.pop()
-        print(url) # for debugging and to see the progress
+        print(url)  # for debugging and to see the progress
 
-        # Save text from the url to a <url>.txt file
-        with open('text/'+local_domain+'/'+url[8:].replace("/", "_") + ".txt", "w") as f:
+        try:
+            filename = 'text/'+local_domain+'/' + \
+                url[8:].replace("/", "_") + ".txt"
 
-            # Get the text from the URL using BeautifulSoup
-            soup = BeautifulSoup(requests.get(url).text, "html.parser")
+            # If the text file already exists, skip it
+            if os.path.exists(filename):
+                continue
 
-            # Get the text but remove the tags
-            text = soup.get_text()
+            # Save text from the url to a <url>.txt file
+            with open(filename, "w", encoding="utf-8") as f:
 
-            # If the crawler gets to a page that requires JavaScript, it will stop the crawl
-            if ("You need to enable JavaScript to run this app." in text):
-                print("Unable to parse page " + url + " due to JavaScript being required")
-            
-            # Otherwise, write the text to the file in the text directory
-            f.write(text)
+                # Get the text from the URL using BeautifulSoup
+                soup = BeautifulSoup(requests.get(url).text, "html.parser")
+
+                # Get the text but remove the tags
+                text = soup.get_text()
+
+                # If the crawler gets to a page that requires JavaScript, it will stop the crawl
+                if ("You need to enable JavaScript to run this app." in text):
+                    print("Unable to parse page " + url +
+                          " due to JavaScript being required")
+
+                # Otherwise, write the text to the file in the text directory
+                f.write(text)
+        except:
+            print("Unable to parse page " + url)
 
         # Get the hyperlinks from the URL and add them to the queue
         for link in get_domain_hyperlinks(local_domain, url):
@@ -156,11 +173,13 @@ def crawl(url):
                 queue.append(link)
                 seen.add(link)
 
+
 crawl(full_url)
 
 ################################################################################
-### Step 5
+# Step 5
 ################################################################################
+
 
 def remove_newlines(serie):
     serie = serie.str.replace('\n', ' ')
@@ -171,24 +190,25 @@ def remove_newlines(serie):
 
 
 ################################################################################
-### Step 6
+# Step 6
 ################################################################################
 
 # Create a list to store the text files
-texts=[]
+texts = []
 
 # Get all the text files in the text directory
 for file in os.listdir("text/" + domain + "/"):
 
     # Open the file and read the text
-    with open("text/" + domain + "/" + file, "r") as f:
+    with open("text/" + domain + "/" + file, "r", encoding="utf-8") as f:
         text = f.read()
 
         # Omit the first 11 lines and the last 4 lines, then replace -, _, and #update with spaces.
-        texts.append((file[11:-4].replace('-',' ').replace('_', ' ').replace('#update',''), text))
+        texts.append(
+            (file[len(domain)+1:-4].replace('-', ' ').replace('_', ' ').replace('#update', ''), text))
 
 # Create a dataframe from the list of texts
-df = pd.DataFrame(texts, columns = ['fname', 'text'])
+df = pd.DataFrame(texts, columns=['fname', 'text'])
 
 # Set the text column to be the raw text with the newlines removed
 df['text'] = df.fname + ". " + remove_newlines(df.text)
@@ -196,7 +216,7 @@ df.to_csv('processed/scraped.csv')
 df.head()
 
 ################################################################################
-### Step 7
+# Step 7
 ################################################################################
 
 # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
@@ -212,20 +232,23 @@ df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
 df.n_tokens.hist()
 
 ################################################################################
-### Step 8
+# Step 8
 ################################################################################
 
 max_tokens = 500
 
 # Function to split the text into chunks of a maximum number of tokens
-def split_into_many(text, max_tokens = max_tokens):
+
+
+def split_into_many(text, max_tokens=max_tokens):
 
     # Split the text into sentences
     sentences = text.split('. ')
 
     # Get the number of tokens for each sentence
-    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
-    
+    n_tokens = [len(tokenizer.encode(" " + sentence))
+                for sentence in sentences]
+
     chunks = []
     tokens_so_far = 0
     chunk = []
@@ -233,7 +256,7 @@ def split_into_many(text, max_tokens = max_tokens):
     # Loop through the sentences and tokens joined together in a tuple
     for sentence, token in zip(sentences, n_tokens):
 
-        # If the number of tokens so far plus the number of tokens in the current sentence is greater 
+        # If the number of tokens so far plus the number of tokens in the current sentence is greater
         # than the max number of tokens, then add the chunk to the list of chunks and reset
         # the chunk and tokens so far
         if tokens_so_far + token > max_tokens:
@@ -241,7 +264,7 @@ def split_into_many(text, max_tokens = max_tokens):
             chunk = []
             tokens_so_far = 0
 
-        # If the number of tokens in the current sentence is greater than the max number of 
+        # If the number of tokens in the current sentence is greater than the max number of
         # tokens, go to the next sentence
         if token > max_tokens:
             continue
@@ -251,7 +274,7 @@ def split_into_many(text, max_tokens = max_tokens):
         tokens_so_far += token + 1
 
     return chunks
-    
+
 
 shortened = []
 
@@ -265,39 +288,41 @@ for row in df.iterrows():
     # If the number of tokens is greater than the max number of tokens, split the text into chunks
     if row[1]['n_tokens'] > max_tokens:
         shortened += split_into_many(row[1]['text'])
-    
+
     # Otherwise, add the text to the list of shortened texts
     else:
-        shortened.append( row[1]['text'] )
+        shortened.append(row[1]['text'])
 
 ################################################################################
-### Step 9
+# Step 9
 ################################################################################
 
-df = pd.DataFrame(shortened, columns = ['text'])
+df = pd.DataFrame(shortened, columns=['text'])
 df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
 df.n_tokens.hist()
 
 ################################################################################
-### Step 10
+# Step 10
 ################################################################################
 
-df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
+df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(
+    input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
 df.to_csv('processed/embeddings.csv')
 df.head()
 
 ################################################################################
-### Step 11
+# Step 11
 ################################################################################
 
-df=pd.read_csv('processed/embeddings.csv', index_col=0)
+df = pd.read_csv('processed/embeddings.csv', index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 df.head()
 
 ################################################################################
-### Step 12
+# Step 12
 ################################################################################
+
 
 def create_context(
     question, df, max_len=1800, size="ada"
@@ -307,30 +332,32 @@ def create_context(
     """
 
     # Get the embeddings for the question
-    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+    q_embeddings = openai.Embedding.create(
+        input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
 
     # Get the distances from the embeddings
-    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
-
+    df['distances'] = distances_from_embeddings(
+        q_embeddings, df['embeddings'].values, distance_metric='cosine')
 
     returns = []
     cur_len = 0
 
     # Sort by distance and add the text to the context until the context is too long
     for i, row in df.sort_values('distances', ascending=True).iterrows():
-        
+
         # Add the length of the text to the current length
         cur_len += row['n_tokens'] + 4
-        
+
         # If the context is too long, break
         if cur_len > max_len:
             break
-        
+
         # Else add it to the text that is being returned
         returns.append(row["text"])
 
     # Return the context
     return "\n\n###\n\n".join(returns)
+
 
 def answer_question(
     df,
@@ -374,9 +401,10 @@ def answer_question(
         return ""
 
 ################################################################################
-### Step 13
+# Step 13
 ################################################################################
 
-print(answer_question(df, question="What day is it?", debug=False))
 
-print(answer_question(df, question="What is our newest embeddings model?"))
+print(answer_question(df, question="What does Infrakraft do?", debug=False))
+
+print(answer_question(df, question="How old is Infrakraft?"))
